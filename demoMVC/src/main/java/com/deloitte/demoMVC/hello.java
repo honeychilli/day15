46dcd123f4986022d04258d1a3037256abc7aff8
pandey0 <arpit242002@gmail.com>
2024-11-04 10:28:35 +0530

    2

--- demoMVC/src/main/java/com/deloitte/demoMVC/hello.java
+++ demoMVC/src/main/java/com/deloitte/demoMVC/hello.java
@@ -1,12 +1,15 @@
 package com.deloitte.demoMVC;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class hello {
+    @Value("${user}")
+    private String user;
     @GetMapping("/")
     public String hello(){
-        return "hello world arpit";
+        return "hello world "+user;
     }
 }
